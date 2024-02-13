bfca0b1c4fda69b815b880d6344ac23df5f6e83f
duynghiadev <duynghia22302@gmail.com>
2024-02-13 09:43:16 +0700

    C# - Bài 1: Hello World!

---
+++ ThanTrieu/LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bấm Ctrl F5 để chạy code và xem log trên màn hình terminal
+            // Chứ mình bấm dấu Start trong IDE nó không ra gì hết
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Duy Nghia");
+        }
+    }
+}
